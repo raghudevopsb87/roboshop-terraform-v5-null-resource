a061783c272dc4047620c0fd16d0172a6c88d273
Raghu K <raghuk.vit@gmail.com>
2025-12-04 07:13:58 +0530

    Roboshop project

---
+++ main.tf
@@ -0,0 +1,42 @@
+resource "aws_instance" "instances" {
+  for_each      = var.components
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
+
+  tags = {
+    Name = each.key
+  }
+
+}
+
+resource "aws_route53_record" "a-records" {
+  for_each      = var.components
+  zone_id = var.zone_id
+  name    = "${each.key}-dev"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instances[each.key].private_ip]
+}
+
+resource "null_resource" "ansible" {
+  for_each      = var.components
+
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      password = "DevOps321"
+      host     = aws_instance.instances[each.key].private_ip
+    }
+
+    inline = [
+      "sudo dnf install python3.13-pip -y",
+      "sudo pip3.11 install ansible",
+      "ansible-pull -i localhost, -U https://github.com/raghudevopsb87/roboshop-ansible-templates.git main.yml -e component=${each.key} -e env=dev"
+    ]
+
+  }
+
+}
+
